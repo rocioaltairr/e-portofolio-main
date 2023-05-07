36fac5fdff65862bd0a6329dd5fbb9862b69553d
rocioaltairr <rocio19940331@gmail.com>
2023-05-07 16:27:50 +0800

    add about

---
+++ src/pages/About/about.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import img1 from './profile-img.jpeg'
+const About = () => {
+    return (
+        <div className='about-outside' >
+            <div style={{padding: '100px', display: 'flex', flexDirection: 'column', alignItems: 'flex-start'}}>
+            <p style={{textAlign: 'left'}}  color="black" size="3">Hi, my name is </p>
+            <font style={{textAlign: 'left'}} color="black" size="8">Rocio Altair</font>
+            <font style={{textAlign: 'left'}} color="black" size="8">I build things for the app</font>
+            <font style={{textAlign: 'left'}} color="black" size="2" >Flutter/iOS Developer mainly develops apps with flutter, with two years experience. Experienced in Swift/SwiftUI and Java. Keep learning about technologies such as Azure, RxSwift. I My goal is to become an integrated App developer.</font>
+            </div>
+            <img className='profile-img'  src={img1} alt="Girl in a jacket"></img>
+        </div>
+        
+    )
+}
+export default About  
---
+++ src/pages/Profile/index.js
@@ -0,0 +1,2 @@
+import { Typography } from "@material-ui/core/styles/createTypography"
+import React from "react";
