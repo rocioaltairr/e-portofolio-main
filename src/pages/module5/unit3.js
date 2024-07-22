f8658623978f981ec07bf34d57b2edeb6ed57bc3
rocioaltairr <Rocio.pai@viewsoinc.com>
2024-07-23 01:34:01 +0400

    modify

--- src/pages/module5/unit3.js
+++ src/pages/module5/unit3.js
@@ -28,20 +28,12 @@ function Unit3({ cardNoteVisibility, toggleNoteCard }) {
             <Card style={{ display: cardNoteVisibility.unit3 ? 'none' : 'block'}}>
                 <Card.Body style={{textAlign : 'left' }}>
                 <SectionBloc
-                        section={"section1"}
-                        title={"Overview and reflection"}
-                        toggleSection={(section) => toggleSection(section)} 
-                        sectionVisibility={sectionVisibility.section1}
-                    />
-                    <div style={{display: sectionVisibility.section1 ? 'none' : 'block', background: 'rgb(237 237 237)', padding:'20px' }} className='card card-block'>
-                    </div>
-                <SectionBloc
-                    section={"section2"}
+                    section={"section1"}
                     title={"Risk Identification Report"}
                     toggleSection={(section) => toggleSection(section)} 
-                    sectionVisibility={sectionVisibility.section2}
+                    sectionVisibility={sectionVisibility.section1}
                 />
-                <div style={{display: sectionVisibility.section2 ? 'none' : 'block', background: 'rgb(237 237 237)', padding:'20px' }} className='card card-block'>
+                <div style={{display: sectionVisibility.section1 ? 'none' : 'block', background: 'rgb(237 237 237)', padding:'20px' }} className='card card-block'>
                     <a  style={{color: '#11ABB0'}} href='https://docs.google.com/document/d/1nxvKT2R1rfV7uhlMCQRu-SyRV9l5ZvxnrI0iCF7VV5Y/edit?usp=sharing' target="_blank">
                     See more about - Risk Identification Report
                     </a>
